3e21627e72cb7029e0b67646ff2717ddf35aa615
Filip <slayer.github@gmail.com>
2025-07-15 01:47:32 +0200

    fix(cmake): added static build and fixed install

--- cmake/modes.cmake
+++ cmake/modes.cmake
@@ -1,2 +1,3 @@
 option(BLACKBASE_BUILD_SHARED "Build BlackBase as a shared library" OFF)
+option(BLACKBASE_BUILD_STATIC "Build BlackBase as a static library" OFF)
 option(BLACKBASE_HEADER_ONLY "Build BlackBase as a header-only library" ON)
