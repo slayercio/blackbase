944eea1994222c82e4dab56820966252e1078584
slayercio <slayer.github@gmail.com>
2025-12-09 00:01:02 +0100

    fix: added macro expansion to the obfuscated macros

--- include/blackbase/obfuscation/value.hpp
+++ include/blackbase/obfuscation/value.hpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <blackbase/xorstr.hpp>
 
-#define OBFUSCATED_VALUE(value) std::strtoull(xorstr_(#value), nullptr, 10)
-#define OBFUSCATED_HVALUE(value) std::strtoull(xorstr_(#value), nullptr, 16)
+// this impl to force macro expansion of value before passing to xorstr_
+#define OBFUSCATED_VALUE_IMPL(value, base) std::strtoull(xorstr_(#value), nullptr, base)
+
+#define OBFUSCATED_VALUE(value) OBFUSCATED_VALUE_IMPL(value, 10)
+#define OBFUSCATED_HVALUE(value) OBFUSCATED_VALUE_IMPL(value, 16)
 
 #define OBFUSCATED_TRUE static_cast<bool>(OBFUSCATED_VALUE(1))
 #define OBFUSCATED_FALSE static_cast<bool>(OBFUSCATED_VALUE(0))
