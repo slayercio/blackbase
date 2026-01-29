1edbb8c72305ef86186e41fb35161db9e615fec4
slayercio <slayer.github@gmail.com>
2026-01-29 19:32:20 +0100

    feat(obfuscation): add more macros

--- include/blackbase/obfuscation/value.hpp
+++ include/blackbase/obfuscation/value.hpp
@@ -4,9 +4,18 @@
 
 // this impl to force macro expansion of value before passing to xorstr_
 #define OBFUSCATED_VALUE_IMPL(value, base) std::strtoull(xorstr_(#value), nullptr, base)
+#define OBFUSCATED_VALUE_IMPL_S(value, base) std::strtoll(xorstr_(#value), nullptr, base)
 
 #define OBFUSCATED_VALUE(value) OBFUSCATED_VALUE_IMPL(value, 10)
 #define OBFUSCATED_HVALUE(value) OBFUSCATED_VALUE_IMPL(value, 16)
 
-#define OBFUSCATED_TRUE static_cast<bool>(OBFUSCATED_VALUE(1))
-#define OBFUSCATED_FALSE static_cast<bool>(OBFUSCATED_VALUE(0))
+#define OBFUSCATED_SVALUE(value) OBFUSCATED_VALUE_IMPL_S(value, 10)
+#define OBFUSCATED_SHVALUE(value) OBFUSCATED_VALUE_IMPL_S(value, 16)
+
+#define OBFUSCATED_ZERO OBFUSCATED_VALUE(0)
+#define OBFUSCATED_ONE OBFUSCATED_VALUE(1)
+
+#define OBFUSCATED_TRUE static_cast<bool>(OBFUSCATED_ONE)
+#define OBFUSCATED_FALSE static_cast<bool>(OBFUSCATED_ZERO)
+
+#define OBFUSCATED_NULLPTR(type) reinterpret_cast<type*>(OBFUSCATED_ZERO)
